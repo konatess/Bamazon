23e26862bd12c08bd1601a3939d498db23757293
Kona Tess <konatess@gmail.com>
2018-10-26 23:01:02 -0700

    Added to gitignore.

---
+++ bamazonCustomer.js
@@ -0,0 +1,7 @@
+// read and display database items
+// prompt user to choose an item for purchase
+// prompt user to choose a number of items
+// analyze number of items vs number in database
+// if not enough items available, kill request and prompt user
+// if enough items available, prompt confirm with total number and total price
+// if confirmed, update database, and display confirmation message with total price
